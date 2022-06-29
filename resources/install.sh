1f9d692841a289a6d1b81107e4e4e6ce6d1f1dfc
Bwibwi13 <bwibwi13@gmail.com>
2022-06-29 17:51:35 +0200

    Update install.sh

--- resources/install.sh
+++ resources/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Ensure required python modules are installed
-sudo python3 -m pip install pyModbusTCP
+python3 -m pip install pyModbusTCP
 if [ $? -ne 0 ]; then
 	echo "Could not install python3 modules - Abort"
 	exit 1
